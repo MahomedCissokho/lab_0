ea9400e4947393e2135c453a240df54e9992accb
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-03-29 12:15:25 +0000

    now adding solution to the exercise

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication { // Renamed main class
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("msg: Car App started! Goto http://localhost:8081/dic_student_name");
 	}
+}
 
+@RestController
+@RequestMapping("/dic_student_name")
+class DicController {
+
+	@Value("${fullname:Bass}")
+	private String fullname;
+
+	@GetMapping
+	String getFullName() {
+		return "Work done by " + this.fullname + ".";
+	}
 }
